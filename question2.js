d2c7efb7a0daa96288ecaf6974643f23d9311ba9
neema <neemabridgetteodire2@gmail.com>
2024-04-03 11:05:27 +0300

    code challenge week2

--- question2.js
+++ question2.js
@@ -1,24 +1,26 @@
 
 function generateArray(begin,finish){
    
-    const solution1=[]
+    const solution1=[];// an array is created
     if(begin<= finish){
-        for(let i = begin;i < finish;i++) ;{
-            return solution1.push(i);} 
+        for(let i = begin;i <= finish;i++){
+             solution1.push(i);} 
         }
+        // Used the for loop to find the elements between the begin and the finsh
     else{
-        for(let i =begin ;i >finish; i--);{
-            return solution1.push(i);
+        for(let i =begin ;i >=finish; i--){
+             solution1.push(i);
         }
     }    
 
-    
-   
+    //use return to get the output as solution1
+   return solution1;
 }
-let array1 = generateArray(4,7)
-let array2 = generateArray(-4,7)
-console.log(array1)
-console.log(array2)
+// call function and display 
+let array1 = generateArray(4,7);
+let array2 = generateArray(-4,7);
+console.log(array1);
+console.log(array2);
 
 
 
